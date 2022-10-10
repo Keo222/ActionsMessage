720155005060cd24318896ff299b8e399d69c3da
Kyle Olsen <kyle.olsen222@gmail.com>
2022-10-10 14:12:37 -0700

    added comments and deleted mistake

--- message.js
+++ message.js
@@ -1,6 +1,8 @@
+// Message that is displayed on screen
+// Updated to become title of most recent Github Issue
 export let message = "First Message";
 message = "Hi!";
-message = "Hello"
-message = "Next trial"
-message = "hi alissa"
-message = "${SANITIZED}"
+message = "Hello";
+message = "Next trial";
+message = "hi alissa";
+message = "${SANITIZED}";
